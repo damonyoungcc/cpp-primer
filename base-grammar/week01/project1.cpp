8f08a6c48151b7d6ebbf3eb6e8ee2971fc61d21b
yangdepu <yangdepp@163.com>
2021-09-29 19:02:09 +0800

    feat: cpp project1

---
+++ base-grammar/week01/project1.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main(int argc, char *argv[])
+{
+  int a, b;
+  if (argc != 3)
+    cout << "Please input two integers" << endl;
+  else
+    cout << "2 * 3 = " << argv[1] + argv[2] << endl;
+
+  return 0;
+}
+
+int charToNumber() {
+  
+}
