d812401bc8213b71c66b0172b97a5469a4300770
yangdepp <yangdepp@163.com>
2022-07-22 18:18:10 +0800

    feat: week06 Lab

---
+++ CPP/week06/Lab/Lab01.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+using namespace std;
+
+void swap_init(int a, int b);
+void swap_init(int *a, int *b);
+
+int main()
+{
+
+  int num1 = 10;
+  int num2 = 20;
+
+  swap_init(num1, num2);
+  cout << "num1 = " << num1 << ", num2 = " << num2 << endl;
+
+  return 0;
+}
+
+void swap_init(int a, int b)
+{
+  int c = a;
+  a = b;
+  b = c;
+}
+
+void swap_pointer(int *a, int *b)
+{
+}
