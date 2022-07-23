2dcc852f025b4ab7d68f917bb48701a35c09d5fd
yangdepp <yangdepp@163.com>
2022-07-23 23:22:03 +0800

    feat: specialization template

---
+++ CPP/week07/examples/04specialization.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+#include <typeinfo>
+
+using namespace std;
+
+template <typename T>
+T sum(T x, T y)
+{
+  cout << "The input type is " << typeid(T).name() << endl;
+  return x + y;
+}
+
+struct Point
+{
+  int x;
+  int y;
+};
+
+// 加间括号就是特例化
+template <>
+Point sum<Point>(Point pt1, Point pt2)
+{
+  cout << "The input type is " << typeid(pt1).name() << endl;
+  Point pt;
+  pt.x = pt1.x + pt2.x;
+  pt.y = pt1.y + pt2.y;
+  return pt;
+}
+
+int main()
+{
+  cout << "sum = " << sum(1, 2) << endl;
+  cout << "sum = " << sum(1.1, 2.2) << endl;
+
+  Point pt1{1, 2};
+  Point pt2{2, 3};
+  Point pt = sum(pt1, pt2);
+  cout << "pt = (" << pt.x << ", " << pt.y << ")" << endl;
+  return 0;
+}
